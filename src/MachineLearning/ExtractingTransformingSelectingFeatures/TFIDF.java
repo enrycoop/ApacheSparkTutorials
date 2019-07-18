ca3fda0d839cc6246b6547b76654c2727347ab69
enricocoppolecchia <s.coppolecchia2@studenti.uniba.it>
2019-07-18 16:50:08 +0200

    word2vec and tfidf added

---
+++ src/MachineLearning/ExtractingTransformingSelectingFeatures/TFIDF.java
@@ -0,0 +1,66 @@
+package MachineLearning.ExtractingTransformingSelectingFeatures;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.sql.SparkSession;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.spark.ml.feature.HashingTF;
+import org.apache.spark.ml.feature.IDF;
+import org.apache.spark.ml.feature.IDFModel;
+import org.apache.spark.ml.feature.Tokenizer;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.Metadata;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
+
+public class TFIDF {
+    public static void main(String[] args) {
+        Logger.getLogger("org").setLevel(Level.ERROR);
+        Logger.getLogger("akka").setLevel(Level.ERROR);
+
+        SparkSession spark = SparkSession.builder()
+                .appName("Application")
+                .master("local[*]")
+                .getOrCreate();
+
+
+        List<Row> data = Arrays.asList(
+                RowFactory.create(0.0, "Hi I heard about Spark"),
+                RowFactory.create(0.0, "I wish Java could use case classes"),
+                RowFactory.create(1.0, "Logistic regression models are neat")
+        );
+
+        StructType schema = new StructType(new StructField[]{
+                new StructField("label", DataTypes.DoubleType, false, Metadata.empty()),
+                new StructField("sentence", DataTypes.StringType, false, Metadata.empty())
+        });
+
+        Dataset<Row> sentenceData = spark.createDataFrame(data,schema);
+
+        // la colonna di output 'words' sarà aggiunta al Dataset
+        Tokenizer tokenizer = new Tokenizer().setInputCol("sentence").setOutputCol("words");
+        Dataset<Row> wordsData = tokenizer.transform(sentenceData);
+
+        int numFeatures = 20;
+        HashingTF hashingTF = new HashingTF()
+                .setInputCol("words")
+                .setOutputCol("rawFeatures")
+                .setNumFeatures(numFeatures);
+
+        Dataset<Row> featuredData = hashingTF.transform(wordsData);
+        IDF idf = new IDF().setInputCol("rawFeatures").setOutputCol("features");
+        IDFModel idfModel = idf.fit(featuredData);
+
+        Dataset<Row> rescaledData = idfModel.transform(featuredData);
+        featuredData.show();
+
+
+    }
+}
---
+++ src/MachineLearning/ExtractingTransformingSelectingFeatures/WordtoVec.java
@@ -0,0 +1,59 @@
+package MachineLearning.ExtractingTransformingSelectingFeatures;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.sql.SparkSession;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.spark.ml.feature.Word2Vec;
+import org.apache.spark.ml.feature.Word2VecModel;
+import org.apache.spark.ml.linalg.Vector;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.*;
+
+import javax.xml.crypto.Data;
+
+public class WordtoVec {
+
+    public static void main(String[] args) {
+        Logger.getLogger("org").setLevel(Level.ERROR);
+        Logger.getLogger("akka").setLevel(Level.ERROR);
+
+        SparkSession spark = SparkSession.builder()
+                .appName("Application")
+                .master("local[*]")
+                .getOrCreate();
+
+        List<Row> data = Arrays.asList(
+                RowFactory.create(Arrays.asList("Hi I heard about Spark".split(" "))),
+                RowFactory.create(Arrays.asList("I wish Java could use case classes".split(" "))),
+                RowFactory.create(Arrays.asList("Logistic regression models are neat".split(" ")))
+        );
+        StructType schema = new StructType(new StructField[]{
+                new StructField("text", new ArrayType(DataTypes.StringType, true), false, Metadata.empty())
+        });
+        Dataset<Row> documentDF = spark.createDataFrame(data, schema);
+
+        Word2Vec word2Vec = new Word2Vec()
+                .setInputCol("text")
+                .setOutputCol("result")
+                .setVectorSize(3)
+                .setMinCount(0);
+
+        Word2VecModel model = word2Vec.fit(documentDF);
+        Dataset<Row> result = model.transform(documentDF);
+
+        for (Row row : result.collectAsList()) {
+            List<String> text = row.getList(0);
+            Vector vector = (Vector) row.get(1);
+            System.out.println("Text: " + text + " => \nVector: " + vector + "\n");
+        }
+
+
+    }
+}
